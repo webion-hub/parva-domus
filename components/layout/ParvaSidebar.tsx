0f7181c0937d758316af2da56a4583f6a2eeee0c
Matteo Budriesi <68251639+matteo2437@users.noreply.github.com>
2023-02-21 18:20:36 +0100

    create sidebar (#2)
    Co-authored-by: Matteo Budriesi <matteo.budriesi@webion.it>

---
+++ components/layout/ParvaSidebar.tsx
@@ -0,0 +1,52 @@
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, SwipeableDrawer } from "@mui/material"
+import { create } from "zustand"
+import { links } from "../lib/layoutLinks"
+
+interface SidebarState {
+  isOpen: boolean,
+  open: () => void,
+  close: () => void,
+  toggle: () => void,
+}
+
+export const useSidebar = create<SidebarState>((set) => ({
+  isOpen: false,
+  open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
+  toggle: () => set(state => ({isOpen: !state.isOpen})),
+}))
+
+export function ParvaSidebar() {
+  const { close, open, isOpen } = useSidebar()
+
+  return (
+    <SwipeableDrawer
+      anchor="right"
+      open={isOpen}
+      onClose={close}
+      onOpen={open}
+      PaperProps={{ sx: { borderRadius: 0 } }}
+    >
+      <List>
+        {links.map((link, index) => (
+          <ListItem
+            key={index} 
+            disablePadding
+          >
+            <ListItemButton
+              href={link.href}
+              onClick={close}
+            >
+              <ListItemIcon>
+                {link.icon}
+              </ListItemIcon>
+              <ListItemText 
+                primary={link.label} 
+              />
+            </ListItemButton>
+          </ListItem>
+        ))} 
+      </List>
+    </SwipeableDrawer>
+  )
+}
