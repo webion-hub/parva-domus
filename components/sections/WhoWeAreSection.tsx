12a91e4962f64a51d2d63e4686eefe7bfab0f902
Matteo Budriesi <matteo.budriesi@webion.it>
2023-02-21 16:09:25 +0100

    fix bug

--- components/sections/WhoWeAreSection.tsx
+++ components/sections/WhoWeAreSection.tsx
@@ -52,7 +52,7 @@ export function WhoWeAreSection(props: BaseSectionProps) {
           Verifichiamo con diversi controlli, la solvibilità dei nostri inquilini garantendo ai proprietari una locazione in piena sicurezza, grazie anche a prodotti assicurativi mirati per le locazioni. 
           <br/>
           <br/>
-          Ci occupiamo della gestione burocratica del contratto di affitto e delle operazioni ad esso connesse (dalle pulizie alla manutenzione ordinaria e straordinaria dell'appartamento), sollevando il locatore da ogni pensiero.Collaboriamo direttamente con le principali aziende presenti sul territorio cittadino e limitrofo (Gruppo ENI, SAIPEM, SKY, Gruppo ospedaliero San Donato e BMW Italia) fornendo alloggi aziendali per i propri dipendenti e collaboratori esterni, anche per brevi periodi.
+          Ci occupiamo della gestione burocratica del contratto di affitto e delle operazioni ad esso connesse (dalle pulizie alla manutenzione ordinaria e straordinaria dell&apos;appartamento), sollevando il locatore da ogni pensiero.Collaboriamo direttamente con le principali aziende presenti sul territorio cittadino e limitrofo (Gruppo ENI, SAIPEM, SKY, Gruppo ospedaliero San Donato e BMW Italia) fornendo alloggi aziendali per i propri dipendenti e collaboratori esterni, anche per brevi periodi.
         </Typography>
       </Stack>
     </Section>
