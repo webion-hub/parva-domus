801e45d0a1d794951400d11c221da1f1d7d24c5c
Matteo Budriesi <matteo.budriesi@webion.it>
2023-02-21 18:19:58 +0100

    create sidebar

--- components/layout/ParvaAppbar.tsx
+++ components/layout/ParvaAppbar.tsx
@@ -1,7 +1,14 @@
-import { AppBar, Button, ButtonBase, Stack, Toolbar, Typography } from "@mui/material";
+import { CallRounded, MenuRounded } from "@mui/icons-material";
+import { AppBar, Button, IconButton, Stack, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
+import { links } from "../lib/layoutLinks";
+import { ParvaSidebar, useSidebar } from './ParvaSidebar'
 
 export function ParvaAppbar() {
+  const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up('md'));
+  const { toggle } = useSidebar()
+
   const [fill, setFill] = useState(false)
 
   useEffect(() => {
@@ -15,73 +22,96 @@ export function ParvaAppbar() {
     setFill(isAfterQuarterScreen)
   }
 
-  return (
-    <AppBar
-      sx={{
-        transitionProperty: 'padding-top, background, box-shadow',
-        transitionDuration: '250ms',
-        borderRadius: 0,
-        paddingTop: fill ? 0 : 2,
-        background: theme => fill 
-          ? theme.palette.background.default 
-          : 'transparent',
-        boxShadow: fill 
-          ? undefined 
-          : 'none',
-      }}
+  const sideBarBtn = 
+    <IconButton
+      color="primary"
+      onClick={toggle}
     >
-      <Toolbar>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{
-            margin: 'auto',
-            width: '100%',
-            maxWidth: theme => theme.layoutMaxWidth?.appbar,
-          }}
-        >
-          <Button href="#">
-            <Typography 
-              variant="h4" 
-              color="text.primary"
-            >
-              Parva Domus
-            </Typography>  
-          </Button>
+      <MenuRounded/>
+    </IconButton>
+
+  const smallContactUsBtn = 
+    <IconButton
+      href="#contact-us"
+      color="primary"
+    >
+      <CallRounded/>
+    </IconButton>
+
+  const bigContactUsBtn =   
+    <Button 
+      variant="contained"
+      href="#contact-us"
+    >
+      Contattaci
+    </Button>
+
+  const contactUsBtn = isMd
+    ? bigContactUsBtn
+    : smallContactUsBtn
+
+  return (
+    <>
+      <AppBar
+        sx={{
+          transitionProperty: 'padding-top, background, box-shadow',
+          transitionDuration: '250ms',
+          borderRadius: 0,
+          paddingTop: fill ? 0 : 2,
+          background: theme => fill 
+            ? theme.palette.background.default 
+            : 'transparent',
+          boxShadow: fill 
+            ? undefined 
+            : 'none',
+        }}
+      >
+        <Toolbar>
           <Stack
             direction="row"
-            spacing={4}
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              margin: 'auto',
+              width: '100%',
+              maxWidth: theme => theme.layoutMaxWidth?.appbar,
+            }}
           >
-            <Button 
-              href="#"  
-            >
-              Home
+            <Button href="#">
+              <Typography 
+                variant={isMd ? "h4" : "h5"} 
+                color="text.primary"
+              >
+                Parva Domus
+              </Typography>  
             </Button>
-            <Button 
-              href="#who-we-are"
+            <Stack
+              direction="row"
+              spacing={{ xs: 1, md: 4 }}
             >
-              Chi siamo
-            </Button>
-            <Button 
-              href="#services"
-            >
-              Servizi
-            </Button>
-            <Button 
-              href="#faq"
-            >
-              FAQ
-            </Button>
-            <Button 
-              variant="contained"
-              href="#contact-us"
-            >
-              Contattaci
-            </Button>
+              {
+                isMd && 
+                <>
+                  {
+                    links.map((link, index) => (
+                      <Button
+                        key={index}
+                        href={link.href} 
+                      >
+                        {link.label}
+                      </Button>
+                    ))
+                  }
+                </>
+              }
+              {contactUsBtn}
+              {!isMd && sideBarBtn}
+            </Stack>
           </Stack>
-        </Stack>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      {!isMd && <ParvaSidebar/>}
+    </>
   )
 }
+
---
+++ components/lib/layoutLinks.tsx
@@ -0,0 +1,24 @@
+import { BusinessRounded, HomeRepairService, HomeRounded, QuizRounded } from "@mui/icons-material";
+
+export const links = [
+  {
+    label: 'Home',
+    icon: <HomeRounded/>,
+    href: '#',
+  },
+  {
+    label: 'Chi siamo',
+    icon: <BusinessRounded/>,
+    href: '#who-we-are',
+  },
+  {
+    label: 'Servizi',
+    icon: <HomeRepairService/>,
+    href: '#services',
+  },
+  {
+    label: 'FAQ',
+    icon: <QuizRounded/>,
+    href: '#faq',
+  }
+]
